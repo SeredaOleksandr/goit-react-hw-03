2771237cdf2cf79a33b104cd93eafa91d4346dca
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-07-13 14:08:09 +0300

    create all components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,17 @@
 import { useState } from "react";
 import "./App.css";
+import ContactForm from "./ContactForm/ContactForm";
+import SearchBox from "./SearchBox/SearchBox";
+import ContactList from "./ContactList/ContactList";
 
 function App() {
   return (
-    <>
-      <div></div>
-    </>
+    <div>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <SearchBox />
+      <ContactList />
+    </div>
   );
 }
 
