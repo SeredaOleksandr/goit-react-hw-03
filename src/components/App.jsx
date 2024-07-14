240c89c8668733c9619e4f0ce6647c2e97a3d76d
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-07-14 10:40:42 +0300

    add 3 step

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,32 @@
-import { React, useState } from "react";
-import s from "./App.module.css";
-import ContactForm from "./ContactForm/ContactForm";
-import SearchBox from "./SearchBox/SearchBox";
-import ContactList from "./ContactList/ContactList";
-import contactData from "../contacts.json";
+import { React, useState } from 'react';
+import s from './App.module.css';
+import ContactForm from './ContactForm/ContactForm';
+import SearchBox from './SearchBox/SearchBox';
+import ContactList from './ContactList/ContactList';
+import contactData from '../contacts.json';
 
 function App() {
-  const [contacts, setContact] = useState(contactData);
-  const [filter, setFilter] = useState("");
+  const [contacts, setContacts] = useState(contactData);
+  const [filter, setFilter] = useState('');
 
-  const handleSearchChange = (event) => {
+  const addContact = newContact => {
+    setContacts(prevContacts => [...prevContacts, newContact]);
+  };
+
+  const handleSearchChange = event => {
     setFilter(event.target.value);
   };
 
   const filteredContacts = () => {
-    return contacts.filter((contact) =>
+    return contacts.filter(contact =>
       contact.name.toLowerCase().includes(filter.toLowerCase())
     );
   };
 
   return (
-    <div>
+    <div className={s.wrapper}>
       <h1 className={s.title}>Phonebook</h1>
-      <ContactForm />
+      <ContactForm addContact={addContact} />
       <SearchBox value={filter} onChange={handleSearchChange} />
       <ContactList contacts={(contacts, filteredContacts())} />
     </div>
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React from 'react';
+import { nanoid } from 'nanoid';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
+import * as Yup from 'yup';
+import s from './ContactForm.module.css';
 
-const ContactForm = () => {
-  return <div></div>;
+const ContactForm = ({ addContact }) => {
+  const initialValues = {
+    name: '',
+    number: '',
+  };
+
+  const validationSchema = Yup.object({
+    name: Yup.string()
+      .min(3, 'Must be at least 3 characters')
+      .max(50, 'Must be 50 characters or less')
+      .required('Required'),
+    number: Yup.string()
+      .min(10, 'Number is too short')
+      .max(12, 'Number is too long')
+      .matches(/^[0-9]+$/, 'Only numbers are accepted here')
+      .required('Required'),
+  });
+
+  const handleSubmit = (values, actions) => {
+    const newContact = { ...values, id: nanoid() };
+    addContact(newContact);
+    actions.resetForm();
+  };
+
+  return (
+    <div className={s.formWrapper}>
+      <Formik
+        initialValues={initialValues}
+        onSubmit={handleSubmit}
+        validationSchema={validationSchema}
+      >
+        <Form className={s.form}>
+          <label className={s.label}>
+            <span>Name</span>
+            <Field className={s.input} name="name" />
+            <ErrorMessage className={s.error} name="name" component="div" />
+          </label>
+          <label className={s.label}>
+            <span>Number</span>
+            <Field className={s.input} name="number" />
+            <ErrorMessage className={s.error} name="number" component="div" />
+          </label>
+          <button type="submit">Add contacts</button>
+        </Form>
+      </Formik>
+    </div>
+  );
 };
 
 export default ContactForm;
